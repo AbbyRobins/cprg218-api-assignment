59342616754166d8ddf17c1fe3476bfd19f14025
AbbyRobins <abigailr2003@hotmail.ca>
2025-03-22 12:44:54 -0600

    Add EventListeners to each button
    Add EventListeners to each button and apply the same API JavaScript to each, aside from category/button names.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-heading.textContent = "Joke of the Day"
+//INITIAL
+
+heading.textContent = "Joke of the Day";
 
 joke.textContent = "Loading . . .";
 
@@ -25,3 +27,168 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
         joke.textContent = "Something went wrong... Please try again."
     }
 });
+
+//BUTTONS
+
+const programmingButton = document.getElementById('programming-button');
+const miscButton = document.getElementById('misc-button');
+const punButton = document.getElementById('pun-button');
+const spookyButton = document.getElementById('spooky-button');
+const christmasButton = document.getElementById('christmas-button');
+
+//EVENTS
+
+//Programming
+
+programmingButton.addEventListener('click', function(){
+
+    heading.textContent = "A Random Programming Joke";
+
+    fetch("https://v2.jokeapi.dev/joke/programming?safe-mode")
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data);
+        console.log(data.setup);
+        console.log(data.delivery);
+        console.log(data.joke);
+        console.log(data.type);
+        
+        if (data.type === 'single'){
+            joke.textContent = data.joke;
+        }
+
+        else if (data.type === 'twopart'){
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+
+        else {
+            joke.textContent = "Something went wrong... Please try again."
+        }
+    })
+})
+
+//Misc
+
+miscButton.addEventListener('click', function(){
+
+    heading.textContent = "A Random Miscellaneous Joke";
+
+    fetch("https://v2.jokeapi.dev/joke/miscellaneous?safe-mode")
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data);
+        console.log(data.setup);
+        console.log(data.delivery);
+        console.log(data.joke);
+        console.log(data.type);
+        
+        if (data.type === 'single'){
+            joke.textContent = data.joke;
+        }
+
+        else if (data.type === 'twopart'){
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+
+        else {
+            joke.textContent = "Something went wrong... Please try again."
+        }
+    })
+})
+
+//Pun
+
+punButton.addEventListener('click', function(){
+
+    heading.textContent = "A Random Pun";
+
+    fetch("https://v2.jokeapi.dev/joke/pun?safe-mode")
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data);
+        console.log(data.setup);
+        console.log(data.delivery);
+        console.log(data.joke);
+        console.log(data.type);
+        
+        if (data.type === 'single'){
+            joke.textContent = data.joke;
+        }
+
+        else if (data.type === 'twopart'){
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+
+        else {
+            joke.textContent = "Something went wrong... Please try again."
+        }
+    })
+})
+
+//Spooky
+
+spookyButton.addEventListener('click', function(){
+
+    heading.textContent = "A Random Spooky Joke";
+
+    fetch("https://v2.jokeapi.dev/joke/spooky?safe-mode")
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data);
+        console.log(data.setup);
+        console.log(data.delivery);
+        console.log(data.joke);
+        console.log(data.type);
+        
+        if (data.type === 'single'){
+            joke.textContent = data.joke;
+        }
+
+        else if (data.type === 'twopart'){
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+
+        else {
+            joke.textContent = "Something went wrong... Please try again."
+        }
+    })
+})
+
+//Christmas
+
+christmasButton.addEventListener('click', function(){
+
+    heading.textContent = "A Random Christmas Joke";
+
+    fetch("https://v2.jokeapi.dev/joke/christmas?safe-mode")
+
+    .then(response => response.json())
+
+    .then(data => {
+        console.log(data);
+        console.log(data.setup);
+        console.log(data.delivery);
+        console.log(data.joke);
+        console.log(data.type);
+        
+        if (data.type === 'single'){
+            joke.textContent = data.joke;
+        }
+
+        else if (data.type === 'twopart'){
+            joke.textContent = data.setup + " ... " + data.delivery;
+        }
+
+        else {
+            joke.textContent = "Something went wrong... Please try again."
+        }
+    })
+})
